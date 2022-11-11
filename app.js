212c71f78b1980a20cb617e3533aeea8c8dc8e0b
ohchocolate <zhongxiaoyao1@gmail.com>
2022-11-11 18:00:26 -0500

    a8

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from "cors";
 import HelloController from "./controller/hello-controller.js";
 import UserController from "./controller/users/users-controller.js";
-import TuitsController from "./controller/tuits/tuits-controller.js";
+//import TuitsController from "./controller/tuits/tuits-controller.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-TuitsController(app);
+
+//TuitsController(app);
+
 HelloController(app);
 UserController(app);
 
